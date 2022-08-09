5742369a31d44b2b7cdc7ff5304d11966e20285d
Tom Scott <tscott@stratanetworks.com>
2022-08-09 14:38:01 -0600

    server is running and returning ok

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 mod server;
 
-fn main() {
-    println!("Hello, world!");
+extern crate log;
+extern crate pretty_env_logger;
+
+#[tokio::main]
+async fn main() {
+    // Init logger
+    pretty_env_logger::init();
+
+    // Start API server
+    server::start(([127, 0, 0, 1], 3030)).await;
 }
